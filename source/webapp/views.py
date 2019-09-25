e0f7aa97b644852fb0dbd7f353f45f2bb88f88ad
k2Raven <leha.21.05.1996@gmail.com>
2019-09-26 02:42:38 +0600

    Создание Главной Страницы

--- source/webapp/views.py
+++ source/webapp/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from django.views.generic import TemplateView
 
-# Create your views here.
+
+from webapp.models import Task
+
+
+class IndexView(TemplateView):
+    template_name = 'index.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['Tasks'] = Task.objects.all()
+        return context
