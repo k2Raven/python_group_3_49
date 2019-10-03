d21d9746299a58de23412dae830e6f3b792f5c1d
k2Raven <leha.21.05.1996@gmail.com>
2019-10-03 18:08:26 +0600

    использовал ListView

--- source/webapp/views/task_view.py
+++ source/webapp/views/task_view.py
@@ -5,18 +5,16 @@ from django.views.generic import View, TemplateView, ListView
 from webapp.forms import TaskForm
 from webapp.models import Task, Status, Types
 
-#
-# class IndexView(ListView):
-#     template_name = 'index.html'
-#     context_object_name = 'Tasks', 'Status', 'Types'
-#     model = Task, Status, Types
-#
-class IndexView(TemplateView):
+
+class IndexView(ListView):
     template_name = 'index.html'
+    context_object_name = 'Tasks'
+
+    def get_queryset(self):
+        return Task.objects.order_by('-created_at')
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['Tasks'] = Task.objects.order_by('-created_at')
         context['Status'] = Status.objects.all()
         context['Types'] = Types.objects.all()
         return context
