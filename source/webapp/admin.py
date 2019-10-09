1a578e3731da49d513d0959bcfde0e4e51c3d8d9
k2Raven <leha.21.05.1996@gmail.com>
2019-10-10 01:23:31 +0600

    Добавил модель проект, добавил в задачи внешний ключь к проекту, создал проекты, прикрепил существующие задачи, обновил фикстуру

--- source/webapp/admin.py
+++ source/webapp/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from webapp.models import Task, Status, Types
+from webapp.models import Task, Status, Types, Project
 
 admin.site.register(Task)
 admin.site.register(Status)
 admin.site.register(Types)
+admin.site.register(Project)
--- source/webapp/models.py
+++ source/webapp/models.py
@@ -1,7 +1,16 @@
 from django.db import models
 
 
+class Project(models.Model):
+    name = models.CharField(max_length=200, verbose_name='Название проекта')
+    description = models.TextField(max_length=3000, null=True, blank=True, verbose_name='Описание проекта')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
+    updated_at = models.DateTimeField(auto_now=True, verbose_name='Дата обновления')
+
+
 class Task(models.Model):
+    project = models.ForeignKey('webapp.Project', related_name='task', on_delete=models.CASCADE,
+                                null=True, blank=False, verbose_name='Проект')
     summary = models.CharField(max_length=200, verbose_name='Краткое описание')
     description = models.TextField(max_length=3000, null=True, blank=True, verbose_name='Полное описание')
     status = models.ForeignKey('webapp.Status', related_name='tasks', on_delete=models.PROTECT, verbose_name='Статус')
