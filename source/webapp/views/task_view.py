cc40a6247c1b47ea6dc3d800db472ad8ddf207a9
k2Raven <leha.21.05.1996@gmail.com>
2019-10-15 22:17:01 +0600

    Добавил поиск в проект на страницу список задач

--- source/webapp/views/task_view.py
+++ source/webapp/views/task_view.py
@@ -1,25 +1,48 @@
 from django.shortcuts import get_object_or_404, redirect
 from django.urls import reverse, reverse_lazy
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
-from django.shortcuts import render
-from webapp.forms import TaskForm
+from django.db.models import Q
+from django.utils.http import urlencode
+
+from webapp.forms import TaskForm, SimpleSearchForm
 from webapp.models import Task, Status, Types, Project
 
 
 class IndexView(ListView):
     template_name = 'index.html'
+    model = Task
     context_object_name = 'Tasks'
+    ordering = ['-created_at']
     paginate_by = 5
     paginate_orphans = 1
 
-    def get_queryset(self):
-        return Task.objects.order_by('-created_at')
+    def get(self, request, *args,**kwargs):
+        self.form = self.get_search_form()
+        self.search_value = self.get_search_value()
+        return super().get(request, *args,**kwargs)
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(object_list=object_list, **kwargs)
         context['Status'] = Status.objects.all()
         context['Types'] = Types.objects.all()
+        context['form'] = self.form
+        if self.search_value:
+            context['query'] = urlencode({'search': self.search_value})
         return context
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        if self.search_value:
+            query = Q(summary__icontains=self.search_value) | Q(description__icontains=self.search_value)
+            queryset = queryset.filter(query)
+        return queryset
+
+    def get_search_form(self):
+        return SimpleSearchForm(self.request.GET)
+
+    def get_search_value(self):
+        if self.form.is_valid():
+            return self.form.cleaned_data['search']
 
 
 class TaskView(DetailView):
