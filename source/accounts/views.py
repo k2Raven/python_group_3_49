892a858d51370d7e563c3449a206af1bdb9607b4
k2Raven <leha.21.05.1996@gmail.com>
2019-10-24 13:32:53 +0600

    Бонус ,перенаправление

--- source/accounts/views.py
+++ source/accounts/views.py
@@ -4,13 +4,15 @@ from django.shortcuts import render, redirect
 
 def login_view(request):
     context = {}
+    redirect_page = request.session.setdefault('redirect_page', request.GET.get('next'))
+
     if request.method == 'POST':
         username = request.POST.get('username')
         password = request.POST.get('password')
         user = authenticate(request, username=username, password=password)
         if user is not None:
             login(request, user)
-            return redirect('webapp:project_list')
+            return redirect(redirect_page)
         else:
             context['has_error'] = True
     return render(request, 'login.html', context=context)
