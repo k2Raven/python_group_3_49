e6dffdda0f0c810db69650cd1c26693554c45800
k2Raven <leha.21.05.1996@gmail.com>
2019-10-28 18:41:00 +0600

    валидация на регистрацию

---
+++ source/accounts/forms.py
@@ -0,0 +1,43 @@
+from django.contrib.auth.models import User
+from django import forms
+from django.core.exceptions import ValidationError
+
+
+class UserCreationForm(forms.ModelForm):
+    password = forms.CharField(label="Пароль", strip=False, widget=forms.PasswordInput)
+    password_confirm = forms.CharField(label="Подтвердите пароль", widget=forms.PasswordInput, strip=False)
+    email = forms.EmailField(label='Email', required=True)
+
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+        try:
+            User.objects.get(email=email)
+            raise ValidationError('User with this email already exists', code='user_email_exists')
+        except User.DoesNotExist:
+            return email
+
+    def clean_password_confirm(self):
+        password = self.cleaned_data.get("password")
+        password_confirm = self.cleaned_data.get("password_confirm")
+        if password and password_confirm and password != password_confirm:
+            raise forms.ValidationError('Пароли не совпадают!')
+        return password_confirm
+
+    def clean(self):
+        super().clean()
+        first_name = self.cleaned_data['first_name']
+        last_name = self.cleaned_data['last_name']
+        if not(first_name or last_name):
+            raise ValidationError('Нужно заполнить хотябы одно поле из first_name или last_name')
+        return self.cleaned_data
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.set_password(self.cleaned_data["password"])
+        if commit:
+            user.save()
+        return user
+
+    class Meta:
+        model = User
+        fields = ['username', 'password', 'password_confirm', 'first_name', 'last_name']
--- source/accounts/views.py
+++ source/accounts/views.py
@@ -1,23 +1,16 @@
-from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth import login
 from django.shortcuts import render, redirect
+from .forms import UserCreationForm
 
 
-def login_view(request):
-    context = {}
-    redirect_page = request.session.setdefault('redirect_page', request.GET.get('next'))
-
+def register_view(request, *args, **kwargs):
     if request.method == 'POST':
-        username = request.POST.get('username')
-        password = request.POST.get('password')
-        user = authenticate(request, username=username, password=password)
-        if user is not None:
+        form = UserCreationForm(data=request.POST)
+        if form.is_valid():
+            user = form.save()
             login(request, user)
-            return redirect(redirect_page)
-        else:
-            context['has_error'] = True
-    return render(request, 'registration/login.html', context=context)
+            return redirect('webapp:project_list')
 
-
-def logout_view(request):
-    logout(request)
-    return redirect('webapp:project_list')
+    else:
+        form = UserCreationForm()
+    return render(request, 'create.html', context={'form': form})
