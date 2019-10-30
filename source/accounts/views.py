62aaa7943c31bd44fca4fa51e1e271a6efa32e9c
k2Raven <leha.21.05.1996@gmail.com>
2019-10-31 02:50:16 +0600

    Добавил редакторование страницы и редактирование пароля

--- source/accounts/views.py
+++ source/accounts/views.py
@@ -1,8 +1,8 @@
 from django.views.generic import ListView, DetailView, UpdateView
 from django.contrib.auth.models import User
 from django.contrib.auth import login
-from django.shortcuts import render, redirect
-from .forms import UserCreationForm
+from django.shortcuts import render, redirect, reverse
+from .forms import UserCreationForm, UserChangeForm, PasswordChangeForm
 
 
 def register_view(request, *args, **kwargs):
@@ -22,3 +22,26 @@ class UserDetailView(DetailView):
     model = User
     template_name = 'user_detail.html'
     context_object_name = 'user_obj'
+
+
+class UserPersonalInfoChangeView(UpdateView):
+    model = User
+    template_name = 'user_info_change.html'
+    form_class = UserChangeForm
+    context_object_name = 'user_obj'
+
+    def test_func(self):
+        return self.get_object() == self.request.user
+
+    def get_success_url(self):
+        return reverse('accounts:detail', kwargs={'pk': self.object.pk})
+
+
+class UserPasswordChangeView(UpdateView):
+    model = User
+    template_name = 'user_password_change.html'
+    form_class = PasswordChangeForm
+    context_object_name = 'user_obj'
+
+    def get_success_url(self):
+        return reverse('accounts:login')
