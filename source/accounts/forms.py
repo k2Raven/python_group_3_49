0657734e2a4876b3bb96ff41d795867a7dd4204d
k2Raven <leha.21.05.1996@gmail.com>
2019-10-31 19:02:31 +0600

    исправил ошибку

--- source/accounts/forms.py
+++ source/accounts/forms.py
@@ -52,31 +52,26 @@ class UserChangeForm(forms.ModelForm):
 
 
 class PasswordChangeForm(forms.ModelForm):
-    password = forms.CharField(max_length=100, required=True, label='New Password',
-                               widget=forms.PasswordInput)
-    password_confirm = forms.CharField(max_length=100, required=True, label='New Password confirm',
-                                       widget=forms.PasswordInput)
-    old_password = forms.CharField(max_length=100, required=True, label='Old Password',
-                                   widget=forms.PasswordInput)
+    password = forms.CharField(label="Новый пароль", strip=False, widget=forms.PasswordInput)
+    password_confirm = forms.CharField(label="Подтвердите пароль", widget=forms.PasswordInput, strip=False)
+    old_password = forms.CharField(label="Старый пароль", strip=False, widget=forms.PasswordInput)
+
+    def clean_password_confirm(self):
+        password = self.cleaned_data.get("password")
+        password_confirm = self.cleaned_data.get("password_confirm")
+        if password and password_confirm and password != password_confirm:
+            raise forms.ValidationError('Пароли не совпадают!')
+        return password_confirm
 
     def clean_old_password(self):
         old_password = self.cleaned_data.get('old_password')
-        user = self.instance
-        if not user.check_password(old_password):
-            raise ValidationError('Invalid password.', code='invalid_password')
+        if not self.instance.check_password(old_password):
+            raise forms.ValidationError('Старый пароль неправильный!')
         return old_password
-
-    def clean(self):
-        super().clean()
-        password_1 = self.cleaned_data.get('password')
-        password_2 = self.cleaned_data.get('password_confirm')
-        if password_1 != password_2:
-            raise ValidationError('Passwords do not match.', code='passwords_do_not_match')
-        return self.cleaned_data
 
     def save(self, commit=True):
         user = self.instance
-        user.set_password(self.cleaned_data.get('password'))
+        user.set_password(self.cleaned_data["password"])
         if commit:
             user.save()
         return user
@@ -84,4 +79,3 @@ class PasswordChangeForm(forms.ModelForm):
     class Meta:
         model = User
         fields = ['password', 'password_confirm', 'old_password']
-
