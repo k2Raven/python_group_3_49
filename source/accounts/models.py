1276d4cd64ee48dfa4cc98bf26599ccd1f118a52
k2Raven <leha.21.05.1996@gmail.com>
2019-11-04 00:13:32 +0600

    Добавил модеть в accounts, и сделал миграцию

--- source/accounts/models.py
+++ source/accounts/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+from django.contrib.auth.models import User
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, related_name='profile', on_delete=models.CASCADE, verbose_name='Пользователь')
+    avatar = models.ImageField(null=True, blank=True, upload_to='user_pics', verbose_name='Аватар')
+    about_me = models.TextField(max_length=3000, null=True, blank=True, verbose_name='О себе')
+    git_http = models.URLField(null=True, blank=True, verbose_name='Профиль на гитхаб')
+
+    def __str__(self):
+        return self.user.get_full_name() + "'s Profile"
+
+    class Meta:
+        verbose_name = 'Профиль'
+        verbose_name_plural = 'Профили'
+
