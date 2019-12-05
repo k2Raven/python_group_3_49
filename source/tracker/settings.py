d66f70562efb2aa9b34cfd22c7fa9f59f3a85aa7
k2Raven <leha.21.05.1996@gmail.com>
2019-12-05 14:58:53 +0600

    создал приложение api_v2

--- source/tracker/settings.py
+++ source/tracker/settings.py
@@ -37,9 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
     'webapp',
     'widget_tweaks',
-    'accounts'
+    'accounts',
+    'api_v2',
 ]
 
 MIDDLEWARE = [
